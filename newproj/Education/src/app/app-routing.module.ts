7f8c11b02280c44eb6c28582d89558aa93dcaa1e
suganya97 <sssuganyaswathi@gmail.com>
2019-09-29 22:39:13 +0530

    mentor

--- newproj/Education/src/app/app-routing.module.ts
+++ newproj/Education/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { CourseComponent } from './course/course.component';
 
 
 const routes: Routes = [
   {path:'',redirectTo:'index',pathMatch:'full'},
   {path:'index',component:HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'contacts',component:ContactsComponent}
+  {path:'contacts',component:ContactsComponent},
+  {path:'courses',component:CourseComponent}
 ];
 
 @NgModule({
